9fb09ad98deee3b7d5210d20765a0d6fd4f3c256
Tonyy107 <antonmagdy53@gmail.com>
2023-04-28 01:16:03 +0000

    lol

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -32,7 +32,7 @@ size_t i = 0;
 
 while (h)
 {
-printf("[%d] %s/n", _strlen(h->str), h->str ? h->str : "(nil)")
+printf("[%d] %s/n", _strlen(h->str), h->str ? h->str : "(nil)");
 h = h->next;
 i++;
 }
