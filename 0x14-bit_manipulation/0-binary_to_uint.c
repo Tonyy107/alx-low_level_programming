6a87cc83d35546115e20f32e5313d0c118ed94b5
Tonyy107 <antonmagdy53@gmail.com>
2023-05-05 02:51:21 +0300

    lol

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -2,26 +2,33 @@
 
 /**
  *binary_to_uint - converts a binary number to an unsigned int
- *@b: the binary nuber as a string
-
- *Return: the unsigned int form of b
+ *Return: Always 0
  */
 
 unsigned int binary_to_uint(const char *b)
 {
-	int len, i;
-	uint sum = 0;
+	unsigned int len, i, j, sum, pow;
+	int base;
 
-	if (!b)
-		return (sum);
+	base = 2;
+	sum = 0;
+	pow = 1;
 
-	len = slen(b);
-	if (!valid(b))
+	if (b == NULL)
 		return (0);
-
-	for (i = 0, len--; b[i]; i++, len--)
-		if (b[i] == '1')
-			sum += 1 * (1 << len);
-
+	for (len = 0; b[len] != '\0'; len++)
+		;
+	if (len == 1 && (b[0] == '0' || b[0] == '1'))
+		return ((b[0] - 48));
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+		for (j = len - 1; j > 0; j--)
+			pow = pow * base;
+		sum = sum + (pow * (b[i] - 48));
+		len--;
+		pow = 1;
+	}
 	return (sum);
 }
