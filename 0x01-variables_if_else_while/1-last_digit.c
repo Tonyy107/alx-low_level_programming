9499a04a0ceeefe99987e0c6b8d646078ad71632
Tonyy107 <antonmagdy53@gmail.com>
2023-07-11 06:07:42 -0700

    lol

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,10 +1,10 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* more headers goes there */
+
 /**
- * main - main function
- * return :  0
+ * main - print somethings 0
+ * return : always  0
  */
 int main(void)
 {
