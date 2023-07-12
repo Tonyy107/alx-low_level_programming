b87316caec75add9d2e7975b48f5e3e498bf4ac2
Tonyy107 <antonmagdy53@gmail.com>
2023-07-12 03:48:49 -0700

    lol

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,10 +10,9 @@ int main(void)
 
 	for (a = 97 ; a <= 122 ; a++)
 	{
-		if (!(a == 101) && !(a == 113))
-		{
+		if ((a == 101) || (a == 113))
+			a++
 			putchar(a);
-		}
 	}
 	putchar('\n');
 	return (0);
