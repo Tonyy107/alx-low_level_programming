6de4326a40487c52f2c74bb9c3fbac9913e2d962
Tonyy107 <antonmagdy53@gmail.com>
2023-07-13 02:36:32 -0700

    lol

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: 0
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 48 ; a <= 57 ; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
