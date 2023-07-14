4d96cc5eeec2846aa337754d7f37605877af5dc3
Tonyy107 <antonmagdy53@gmail.com>
2023-07-13 18:29:45 -0700

    lol

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: 0
+ */
+int main(void)
+{
+	int x, y, z;
+
+	for (x = 48 ; x <= 57 ; x++)
+	{
+		for (y = 49 + (x - 48) ; y <= 57 ; y++)
+		{
+			for (z = 50 + (y - 48) ; z <= 57 ; z++)
+			{
+				putchar(x);
+				putchar(y);
+				if (x != 55)
+				{
+					putchar(44);
+					putchar(32);
+				}
+			}
+		}
+	putchar('\n');
+	return (0);
+}
