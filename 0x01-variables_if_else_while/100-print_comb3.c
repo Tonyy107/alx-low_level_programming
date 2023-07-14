859ead0d0e46b573f1582c4c1a8c37556f0ef952
Tonyy107 <antonmagdy53@gmail.com>
2023-07-13 18:00:10 -0700

    lol

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: 0
+ */
+int main(void)
+{
+	int x, y;
+
+	for (x = 48 ; x <= 57 ; x++)
+	{
+		putchar(x);
+		for (y = 48 + x ; y <= 57 ; y++)
+		{
+			putchar(x);
+			putchar(44);
+			putchar(32);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
