0aa815dc16e72758f4c7fc0eb629bd84260fb0ec
Tonyy107 <antonmagdy53@gmail.com>
2024-03-12 21:02:08 +0200

    Update function to handle edge cases

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * print_rev - prints a string in reverse
+ * @s: string to be printed
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+	int counter = 0;
+
+	while (s[counter] != '\0')
+	{
+		counter++;
+	}
+	for (; counter >= 0; counter--)
+	{
+		_putchar(s[counter]);
+	}
+}
