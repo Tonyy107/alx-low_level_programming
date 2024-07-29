84f8053f9bf36f1e090415ee104408d85634bc24
Tonyy107 <antonmagdy53@gmail.com>
2024-07-29 21:04:38 +0300

    Refactor code in 0-create_array.c for consistent indentation and correct header inclusion

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,6 +1,14 @@
 #include "main.h"
 #include <stdlib.h>
 
+/**
+ * create_array - Creates an array of characters and initializes it with a specific character
+ * @size: The size of the array to be created
+ * @c: The character to initialize the array with
+ *
+ * Return: If size is 0 or if memory allocation fails, returns NULL.
+ *         Otherwise, returns a pointer to the newly created array.
+ */
 char *create_array(unsigned int size, char c)
 {
 	char *arr = (char *)malloc(size * sizeof(char));
