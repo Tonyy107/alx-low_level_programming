4dbf17dc508e585f96cf316ded1e201e1e929689
Tonyy107 <antonmagdy53@gmail.com>
2024-07-29 21:00:28 +0300

    Refactor code to use consistent indentation and include correct header file in 0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -4,6 +4,7 @@
 char *create_array(unsigned int size, char c)
 {
     char *arr = (char *)malloc(size * sizeof(char));
+    unsigned int i;
     if (size == 0)
     {
         return NULL;
@@ -14,7 +15,7 @@ char *create_array(unsigned int size, char c)
     }
     else
     {
-        for (unsigned int i = 0; i < size; i++)
+        for (i = 0; i < size; i++)
         {
             arr[i] = c;
         }
