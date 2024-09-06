0c926519d9a297bff42f18cc0c5cfa42a0b18bdf
Tonyy107 <antonmagdy53@gmail.com>
2024-09-06 15:47:59 +0300

    Refactor init_dog function for consistent indentation and correct header inclusion

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,5 +1,13 @@
 #include "dog.h"
-
+/**
+ * init_dog - initializes a dog
+ * @d: the dog to init
+ * @name: the dog's name
+ * @age: dog's age
+ * @owner: owner's name
+ *
+ * return: void.
+ */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
     d->name = name;
