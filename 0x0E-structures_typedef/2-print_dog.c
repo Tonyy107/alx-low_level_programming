64467cafba65fd0d0ebbe5c4141240eeb670ca49
Tonyy107 <antonmagdy53@gmail.com>
2024-09-06 16:50:43 +0300

    Refactor print_dog function for consistent indentation and correct handling of NULL values

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -10,27 +10,20 @@ void print_dog(struct dog *d)
 {
 	if (d)
 	{
-		if (d->name!=NULL)
+		if (d->name != NULL)
 		{
 			printf("Name: %s\n", d->name);
 		}
-		else
+		else if (d->name == NULL)
 		{
 			printf("Name: %s\n", "(nil)");
 		}
-		if (d->age != 0)
-		{
-			printf("Age: %f\n", d->age);
-		}
-		else
-		{
-			printf("Age: %s\n", "(nil)");
-		}
-		if (d->owner!=NULL)
+		printf("Age: %f\n", d->age);
+		if (d->owner != NULL)
 		{
 			printf("Owner: %s\n", d->owner);
 		}
-		else
+		else if (d->owner == NULL)
 		{
 			printf("Owner: %s\n", "(nil)");
 		}
