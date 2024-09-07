39c0ff154487c82a324dc2a9aa211a68bb8cce03
Tonyy107 <antonmagdy53@gmail.com>
2024-09-07 14:09:19 +0300

    Refactor new_dog function to handle memory allocation failure and improve error messages

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "dog.h"
+
 /**
  * new_dog - creates a new dog
  * @name: name of dog
  * @age: age of dog
  * @owner: owner of dog
  *
- * Return: pointer to new dog
+ * Return: pointer to new dog, or NULL if it fails
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
@@ -15,12 +17,28 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	if (dog == NULL)
 	{
-		printf("Memory allocation failed\n");
+		printf("Memory allocation for dog structure failed\n");
 		return (NULL);
 	}
 
-	dog->name = name;
+	dog->name = strdup(name);
+	if (dog->name == NULL)
+	{
+		printf("Memory allocation for name failed\n");
+		free(dog);
+		return (NULL);
+	}
+
+	dog->owner = strdup(owner);
+	if (dog->owner == NULL)
+	{
+		printf("Memory allocation for owner failed\n");
+		free(dog->name);
+		free(dog);
+		return (NULL);
+	}
+
 	dog->age = age;
-	dog->owner = owner;
+
 	return (dog);
 }
