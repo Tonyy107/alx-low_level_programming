a8a9e6360971e80e99712d3bb9b04d582c931987
Tonyy107 <antonmagdy53@gmail.com>
2024-09-08 13:07:36 +0300

    Refactor lists.h to add missing include guard and print_list function

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,5 +2,23 @@
 /**
  * print_list - function that prints all the elements of a list_t list.
  * @h: it's h dude
-*/
-size_t print_list(const list_t *h);
+ * Return: size_t number of elements
+ */
+size_t print_list(const list_t *h)
+{
+	size_t i = 0;
+
+	for (; h; h = h->next)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%u] %s\n", h->len, h->str);
+		}
+		i++;
+	}
+	return (i);
+}
