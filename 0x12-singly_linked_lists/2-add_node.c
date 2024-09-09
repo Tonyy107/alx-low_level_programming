55932056470fe83aac7f9d028fca615ec8ad6097
Tonyy107 <antonmagdy53@gmail.com>
2024-09-09 18:11:37 +0300

    Refactor add_node function to properly add a new node to the beginning of the list

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,9 +1,15 @@
 #include "lists.h"
 #include <string.h>
-
+/**
+ * add_node - function that adds a new node at the beginning of a list_t list.
+ * @head: it's head dude
+ * @str: string to add in node
+ * Return: the address of the new element
+ */
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new = malloc(sizeof(list_t));
+
 	if (!new)
 	{
 		return (NULL);
