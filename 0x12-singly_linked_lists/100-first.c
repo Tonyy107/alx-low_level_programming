83b41d9c5f61ecf61c026f54a53f4cee428d0b22
Tonyy107 <antonmagdy53@gmail.com>
2024-09-17 16:16:05 +0300

    Refactor main function to print a message before executing

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * before_main - function that prints before the main function is executed.
+ * Return: nothing
+ */
+__attribute__((constructor))
+void before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("bore my house upon my back!\n");
+}
