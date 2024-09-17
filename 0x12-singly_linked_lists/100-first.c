5cd8443b9f77cf6c6fb46f58c6c85c75e52cad0f
Tonyy107 <antonmagdy53@gmail.com>
2024-09-17 16:17:09 +0300

    Refactor print message in before_main function

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -7,5 +7,5 @@ __attribute__((constructor))
 void before_main(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
-	printf("bore my house upon my back!\n");
+	printf("I bore my house upon my back!\n");
 }
