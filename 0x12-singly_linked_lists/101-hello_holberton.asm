9dea7b321a7b3a58ea73ff0c10d5f743f5beda37
Tonyy107 <antonmagdy53@gmail.com>
2024-09-17 17:39:16 +0300

    Refactor print message in before_main function to include a newline character

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,5 +1,5 @@
 section .data
-    fmt db "Hello, Holberton", 0   ; Null-terminated string
+    fmt db "Hello, Holberton\n", 0   ; Null-terminated string
 
 section .text
     global main                     ; Declare main as the entry point
