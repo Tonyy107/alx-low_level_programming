a9172e18bbfa4be04de3d6be08d0f8a60c505052
Tonyy107 <antonmagdy53@gmail.com>
2024-09-17 14:17:36 +0300

    Refactor free_list function to properly free memory in the list_t list

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,1 +1,20 @@
 #include "lists.h"
+/**
+ * free_list - function that frees a list_t list.
+ * @head: list_t list to free
+ * Return: nothing
+ */
+void free_list(list_t *head)
+{
+	if (head)
+	{
+		list_t *temp;
+		while (head != NULL)
+		{
+			temp = head;
+			head = head->next;
+			free(temp->str);
+			free(temp);
+		}
+	}
+}
