b01426a0b9fa5f7aa51ccff841513655d43c14ce
Tonyy107 <antonmagdy53@gmail.com>
2024-09-18 13:39:32 +0300

    Refactor listint_t struct in lists.h and add listint_len function

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * listint_len - function that returns the number of elements in a linked list.
+ * @h: the list
+ * Return: the number of nodes
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t n;
+
+	while (h)
+	{
+		h = h->next;
+		n++;
+	}
+	return (n);
+}
