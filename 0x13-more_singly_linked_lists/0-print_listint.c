669c7ae187e8eb314eba5395819be755316bb051
Tonyy107 <antonmagdy53@gmail.com>
2024-09-18 13:30:38 +0300

    Refactor print message in before_main function to include a newline character Refactor print message in before_main function Refactor main function to print a message before executing Delete unnecessary files and functions Update print_listint function to print elements of a linked list Add main function to test print_listint function

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,21 +1,18 @@
 #include "lists.h"
-
 /**
- * print_listint - prints all the elements of a linked list
- * @h: linked list of type listint_t to print
- *
- * Return: number of nodes
+ * print_listint - function that prints all the elements of a listint_t list.
+ * @h: the list that we want to print his elements.
+ * Return: size_t the number of nodes
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t num = 0;
+	size_t n = 0;
 
 	while (h)
 	{
-		printf("%d\n", h->n);
-		num++;
+		printf("%i\n", h->n);
 		h = h->next;
+		n++;
 	}
-
-	return (num);
+	return (n);
 }
