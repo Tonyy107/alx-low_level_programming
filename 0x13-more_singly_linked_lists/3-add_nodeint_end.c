97b0e34ea95b60e16dfc7918e1e9051579d92901
Tonyy107 <antonmagdy53@gmail.com>
2024-09-25 15:46:29 +0300

    Refactor lists.h to include add_nodeint_end function

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+/**
+ * add_nodeint_end - function that adds a new node at the end of a list.
+ * @head: the list
+ * @n: the number that will add inside the node
+ * Return: the address of the new element, or NULL if it failed
+ */
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *temp = *head;
+	listint_t *new = malloc(sizeof(listint_t));
+
+	new->n = n;
+	new->next = NULL;
+	if (temp == NULL)
+	{
+		*head = new;
+		return (new);
+	}
+	while (temp->next)
+	{
+		temp = temp->next;
+	}
+	temp->next = new;
+	return (new);
+}
